086c8d399ca95e7be78fddd5522511d4a3ff07f7
Chris123tina <christinajacob347@gmail.com>
2025-12-02 13:49:26 +0100

    Update GreenAction.java

--- Clovervile/src/model/GreenAction.java
+++ Clovervile/src/model/GreenAction.java
@@ -1,9 +1,25 @@
 package model;
 
+import java.io.Serializable;
+
+
+/**
+ * A class representing a Trade Offer with a offer name, point cost and the seller.
+ * @author Christina Jacob
+ * @version 1.0
+ */
+
 public class GreenAction
 {
   private String name;
   private int greenPoints;
+
+
+  /**
+   * Three-argument constructor.
+   * @param name the name of the green action
+   * @param greenPoints the points gained by doing green action
+   */
 
   public GreenAction(String name, int greenPoints)
   {
@@ -11,30 +27,51 @@ public class GreenAction
     this.greenPoints = greenPoints;
   }
 
+  /**
+   * return the name of the green action
+   * @return name
+   */
   public String getName()
   {
     return name;
   }
 
+  /**
+   * returns the amount of green points gained by doing green action
+   * @return greenPoints
+   */
   public int getGreenPoints()
   {
     return greenPoints;
   }
+
+  /**
+   * sets the name of the green action
+   * @param name the name of the green action
+   */
 
   public void setName(String name)
   {
     this.name = name;
   }
 
+  /**
+   * sets the amount of points gained by doing the green actions
+   * @param greenPoints the green points gained by doing the green actions
+   */
   public void setGreenPoints(int greenPoints)
   {
     this.greenPoints = greenPoints;
   }
 
+  /**
+   * returns a string representation of green action
+   * @return a string representation of the TradeOffer in the format:
+   */
   public String toString()
   {
-    return "Name of the green action is " + name +
-        "Gained points is " + greenPoints;
+    return   name + " "
+          + greenPoints;
   }
 
   public boolean equals(Object obj)
