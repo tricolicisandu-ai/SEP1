e17a118fa0255fd80eb8572ea846fe426b02e2fe
Chris123tina <christinajacob347@gmail.com>
2025-12-07 15:06:58 +0100

    uu

--- Clovervile/src/view/GreenActions/AddGreenActionController.java
+++ Clovervile/src/view/GreenActions/AddGreenActionController.java
@@ -3,8 +3,11 @@ package view.GreenActions;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import model.CloverVilleModelManager;
 import model.GreenAction;
+import model.GreenActionList;
+
+
 
 
 
@@ -14,18 +17,32 @@ public class AddGreenActionController
   @FXML private  TextField PointField;
 
   private GreenAction newGreenAction;
+  private CloverVilleModelManager model;
 
-  @FXML private void handleAdd (ActionEvent event)
+
+
+  public void set(CloverVilleModelManager model)
   {
-
+    this.model = model;
+  }
+   @FXML private void AddGreenAction (ActionEvent event)
+  {
     try
     {
-      String GreenTask = GreenTaskField.getText().trim();
-      String Point = PointField.getText().trim();
+      String name = GreenTaskField.getText().trim();
+      int point = Integer.parseInt(PointField.getText().trim());
+
+      newGreenAction = new GreenAction(name, point);
+
+      GreenActionList greenActions = model.getAllGreenActions();
+
+      greenActions.addGreenAction(newGreenAction);
+      model.saveGreenActions(greenActions);
+
     }
     catch (Exception e)
     {
-      System.out.println("Invalid input: " + e.getMessage());
+      System.out.println("Error saving green action: " + e.getMessage());
     }
   }
 }
--- Clovervile/src/view/GreenActions/MainGreenActionController.java
+++ Clovervile/src/view/GreenActions/MainGreenActionController.java
@@ -1,5 +1,22 @@
 package view.GreenActions;
 
-public class MainGreenActionController
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainGreenActionController extends Application
 {
+  public void start(Stage window) throws IOException
+  {
+    window.setTitle("MainGreenActionView.FXML");
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("AddGreenActionView.fxml"));
+    Scene scene = new Scene(loader.load());
+    window.setScene(scene);
+    window.show();
+  }
 }
+
