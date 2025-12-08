c057b73141871085d78f47748bce68122300907c
Yuliia1605 <juliailienko1992@gmail.com>
2025-12-08 13:04:58 +0100

    yuliia

--- Clovervile/src/view/Resident/AddResidentViewController.java
+++ Clovervile/src/view/Resident/AddResidentViewController.java
@@ -1,25 +1,46 @@
 package view.Resident;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import model.ResidentList;
 
 import javax.swing.*;
 
 public class AddResidentViewController
 {
-  public TextField firstName;
-  public TextField lastName;
-  public TextField points;
+  @FXML private TextField firstName;
+  @FXML private TextField lastName;
+  @FXML private TextField points;
+  private RedentList resdentList;
+  private Object showError;
 
   public void initialize()
   {
 
     firstName.setText("Anna");
+    lastName.setText("Olsen");
+    points.setText("0");
 
   }
-  public void AddResident(ActionEvent actionEvent)
+
+  public void setResdentList(ResidentList resdentList)
   {
-    JOptionPane.showMessageDialog(null, "Do you want to add?");
+    this.resdentList = resdentList;
+  }
+
+  @FXML private void AddResident(ActionEvent actionEvent)
+  {
+    String firstName = firstName.getText();
+    String lastName = lastName.getText();
+    String points = points.getText();
+
+    if(firstName.isEmpty() || lastName.isEmpty() || points.isEmpty() )
+    {
+      showError("All fields must be filled out")
+      return;
+    }
+
   }
 
   public void CancelResident(ActionEvent actionEvent)
