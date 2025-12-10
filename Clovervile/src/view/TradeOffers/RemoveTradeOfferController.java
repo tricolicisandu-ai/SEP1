33acc6304c8d89485a7bf5562ff9d82d5417af21
Sandu Tricolici <tricolicisandu@gmail.com>
2025-12-10 13:37:16 +0100

    vdf

--- Clovervile/src/view/TradeOffers/RemoveTradeOfferController.java
+++ Clovervile/src/view/TradeOffers/RemoveTradeOfferController.java
@@ -1,55 +1,6 @@
 package view.TradeOffers;
 
-import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ComboBox;
-import model.*;
-import view.ViewHandler;
-import javafx.event.ActionEvent;
-import  model.TradeOfferList;
-import model.TradeOffer;
-
-import static javafx.scene.control.skin.TableSkinUtils.getSelectionModel;
-
 public class RemoveTradeOfferController
 {
-  private Scene scene;
-  private CloverVilleModelManager modelManager;
-  private ViewHandler viewHandler;
-
-  @FXML private ComboBox<TradeOffer> tradeOfferComboBox;
-  @FXML private Button removeButton;
-}
-
-public void init(ViewHandler viewHandler, Scene scene, CloverVilleModelManager modelManager)
-{
-  this.scene = scene;
-  this.viewHandler = viewHandler;
-  this.modelManager = modelManager;
-}
-
-public void handleRemove(ActionEvent e)
-{
-  Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
-      "Do you really want to remove?", ButtonType.YES, ButtonType.NO);
-  alert.setTitle("Exit");
-  alert.setHeaderText(null);
-
-  alert.showAndWait();
-
-  if (alert.getResult() == ButtonType.YES)
-  {
-    TradeOffer tradeOffer = tradeOfferComboBox.getSelectionModel().getSelectedItem();
-    modelManager.removeTradeOffer(tradeOffer);
-    updateTradeOfferComboBox();
-  }
-
-  else if (e.getSource() == tradeOfferComboBox)
-  {
-    TradeOffer temp = tradeOfferComboBox.getSelectionModel().getSelectedItem();
-
-  }
+  
 }
